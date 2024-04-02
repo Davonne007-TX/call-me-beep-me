a459c79c4731438b3c3fa96c75e96046c158a29f
Vonney <davonnejv@gmail.com>
2024-04-02 15:34:23 -0500

    final styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 export default function Header() {
   return (
     <header className="bg-[url('images/star.jpg')] bg-cover h-screen flex flex-col justify-center items-center text-white">
-      <div className="ml-auto mr-72 mt-66 text-neutral-100">
-        <h1 className="text-5xl font-serif">Davonne J. Vigil </h1>
-        <br />
-        <p className="mb-10 text-2xl">
+      <div className="ml-auto mr-72 mt-66 text-white">
+        <h1 className="text-5xl font-sans mb-4 font-bold">Davonne Vigil </h1>
+
+        <p className="mb-4 text-2xl">
           Frontend Web Developer
           <br /> React | JavaScript ES6 | HTML5 | CSS3 | Tailwind CSS
         </p>
-        <section className="flex flex-wrap gap-10 font-serif text-xl">
+        <section className="flex flex-wrap gap-10 font-serif text-lg">
           <p>davonnevigil@gmail.com</p>
           <p>linkedin.com/in/davonnevigil/</p>
           <p>github.com/Davonne007-TX</p>

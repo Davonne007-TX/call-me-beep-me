dc6eabfca90b2e36bb6dbad25532a25f801c8f2d
Vonney <davonnejv@gmail.com>
2024-05-27 22:31:19 -0500

    updated UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,34 @@
 export default function Header() {
   return (
-    <header className="bg-[url('images/star.jpg')] bg-cover h-screen flex flex-col justify-center items-center text-white">
-      <div className="ml-auto mr-72 mt-66 text-white">
-        <h1 className="text-5xl font-sans mb-4 font-bold">Davonne Vigil </h1>
-
-        <p className="mb-4 text-2xl">
-          Frontend Web Developer
-          <br /> React | JavaScript ES6 | HTML5 | CSS3 | Tailwind CSS
+    <header className="bg-[url('images/cube.jpg')] bg-cover h-screen flex flex-col justify-center items-center text-white">
+      <p className="text-transparent">
+        Photo by{" "}
+        <a href="https://unsplash.com/@czapp_arpad?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+          Árpád Czapp
+        </a>{" "}
+        on{" "}
+        <a href="https://unsplash.com/photos/a-rubik-cube-sitting-on-top-of-a-laptop-computer-n7zuAz_wwCY?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+          Unsplash
+        </a>
+      </p>
+      /*{" "}
+      <section className="mt-64  ml-auto mr-10  text-orange-100 p-8 rounded-xl max-w-4sxl font-sans">
+        <h1 className="text-3xl font-bold text-red-500">
+          Frontend Web Developer: Davonne Vigil
+        </h1>
+        <p className="text-2xl mt-1">
+          React | JavaScript ES6 | HTML5 | CSS3 | Tailwind CSS
         </p>
-        <section className="flex flex-wrap gap-10 font-serif text-lg">
+        <section className="flex flex-row gap-4 text-xl mt-4 font-serif">
           <p>davonnevigil@gmail.com</p>
           <p>linkedin.com/in/davonnevigil/</p>
           <p>github.com/Davonne007-TX</p>
         </section>
-      </div>
+      </section>{" "}
+      */
     </header>
   );
+}
+
+{
 }

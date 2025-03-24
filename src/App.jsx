a96a51db10a64958ded5de17c53fcb578c6c5723
Davonne Vigil <davonnejv@gmail.com>
2025-03-24 17:14:07 -0500

    feature: header with styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <main className="bg-[url('/images/rubik.jpg')] bg-cover min-h-screen">
+    <main className="bg-[url('/images/lapTop.jpg')] bg-cover min-h-screen flex">
       <Header />
     </main>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,19 @@
-import React from "react";
-
 export default function Header() {
+  const contactMe = [
+    { getInTouch: "linkedin.com/in/davonnevigil ", id: 1 },
+    { getInTouch: "davonnevigil@gmail.com", id: 2 },
+    { getInTouch: "github.com/Davonne007-TX" },
+  ];
   return (
-    <div>
-      <h1 className="font-bold text-7xl text-red-500">Hello Moto</h1>
-    </div>
+    <header className="flex flex-col font-serif text-white ml-auto mt-40">
+      <div className="flex flex-col p-2 mr-4">
+        <h1 className="font-mono text-4xl mt-40 max-w-sm">Davonne Vigil</h1>
+        <ul className="flex gap-8 mt-2 text-lg">
+          {contactMe.map((contact) => (
+            <li key={contact.id}>{contact.getInTouch}</li>
+          ))}
+        </ul>
+      </div>
+    </header>
   );
 }

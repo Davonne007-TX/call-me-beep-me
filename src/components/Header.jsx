1d066ae7c3644cc5cea351ae48997ee9ae66151f
Davonne Vigil <davonnejv@gmail.com>
2025-03-24 18:19:20 -0500

    update banner

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,11 +8,12 @@ export default function Header() {
   ];
   return (
     <header className="flex flex-col font-serif text-white ml-auto mt-40">
-      <div className="flex flex-col p-2 mr-4">
+      <div className="flex flex-col p-2">
         <div className="flex gap-4">
           <h1 className="text-4xl font-mono mt-40 max-w-sm">Davonne Vigil</h1>
           <MyHobbies />
         </div>
+        <h2 className="font-mono text-2xl mt-2">Frontend Web Developer</h2>
         <ul className="flex gap-8 mt-2 text-lg">
           {contactMe.map((contact) => (
             <li key={contact.id}>{contact.getInTouch}</li>
--- src/components/MyHobbies.jsx
+++ src/components/MyHobbies.jsx
@@ -2,7 +2,7 @@ export default function MyHobbies() {
   return (
     <>
       <img src="images/rubik.png" alt="" className="w-40 h-40" />
-      <img src="images/planet-earth.png" alt="" className="w-40 h-40" />
+      {/* <img src="images/planet-earth.png" alt="" className="w-40 h-40" /> */}
     </>
   );
 }
